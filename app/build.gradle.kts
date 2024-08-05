02424b5984d71a7fd5adddc1f5b6401accf0c61b
Yulia Baranova <yulia.gorelikova@yandex.ru>
2024-08-05 18:03:59 +0300

    preparations

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    application
+    checkstyle
+    jacoco
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports { xml.required.set(true) }
+}
